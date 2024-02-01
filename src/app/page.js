5e0ebcd5f68ec98e5bb9488cbc7eefef294dc5c6
Diego <dhiego1510@gmail.com>
2024-02-01 16:53:31 -0500

    improving logic, connecting with backend, I render only the content of my database in 'home'

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,34 @@
-import { loadData } from "@/lib/client";
+'use client'
+import { useEffect, useState } from "react";
 import CountryCard from "./CountryCard";
 
-async function HomePage() {
-  const countries = await loadData()
+function HomePage() {
+  const [countries, setCountries] = useState([])
+  const [inputValue, setInputValue] = useState("")
+
+  useEffect(() => {
+    fetch('http://localhost:3001')
+    .then(res => res.json())
+    .then(data => setCountries(data))
+  }, [])
+
+  console.log(countries);
+
   return (
     <>
       <div>
         <div>
-          <input className="search-input" type="text" placeholder="Encuentra tu pais"></input>
+          <input className="search-input" type="text" placeholder="Encuentra tu pais" onChange={(e) => setInputValue(e.target.value)}></input>
         </div>
-
         <section className="countries-grid">
-          {countries.map(country => (
-            <CountryCard country={country} key={country.code} />
-          ))}
+          
+          {countries.map((country) => {
+            if (country.name.toLowerCase().startsWith(inputValue.toLowerCase())) {
+              return <CountryCard country={country} key={country.code}/>
+            }else {
+              return null
+            }
+          })}
         </section>
       </div>
     </>
---
+++ src/app/server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const port = 3001;
+const mongo = require('./database.js')
+const { Country } = require("./schema.js")
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", async (req, res) =>{
+
+  try {
+    
+    const countries = await Country.find( {} )
+    res.json(countries)
+  }catch (error) {
+    console.error(error);
+    res.status(500).send("Error al obtener los países");
+  }
+  
+});
+
+// app.get("/continent/:code", (req, res) =>{
+//   res.send("Hola mi server en Express");
+// });
+
+app.post("/", (req, res) =>{
+  const body = req.body
+  Country.create(body)
+  res.json(body)
+})
+
+app.put("/code:", async(req, res) => {
+
+  try {
+    
+  const code = req.params.code;
+  const updateData = req.body;
+  const update = await Country.updateOne({ "code": code }, { $set: updateData });
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+app.delete("/code:", (req, res) => {
+
+})
+
+mongo();
+
+
+app.listen(port, () =>{
+  console.log("My port: " + port);
+});
