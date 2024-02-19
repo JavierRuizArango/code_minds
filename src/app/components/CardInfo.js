658403cd83a9dacb30d3e03f1644f8f5a382931c
Diego <dhiego1510@gmail.com>
2024-02-19 06:52:46 -0500

    creating navigation , using pixaby api to load images referring to each country

--- src/app/components/CardInfo.js
+++ src/app/components/CardInfo.js
@@ -1,30 +1,74 @@
 import React from 'react'
-
-const imageStyles = {
-  position: 'relative',
-  width: '50%',
-  height: '180px',
+import { useEffect, useState } from "react";
+ 
+const imageStyles2 = {
+   
+  width: '100%',
+  
 };
 
-const CardInfo = () => {
+const CardInfo = ( {country, close} ) => {
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    const API_KEY = '42373212-afb7a6136c21eddaf8f4e3382';
+    const countryName = country.name.replace(/\s+/g, '+'); // Reemplaza los espacios en blanco con '+' para el formato de URL
+    const category = 'places, city'; 
+    const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent( countryName)}&image_type=photo&category=${category}   `;
+
+    fetch(URL)
+      .then(response => response.json())
+      .then(data => {
+        if (parseInt(data.totalHits) > 0) {
+            // Iterar sobre los resultados para encontrar la primera imagen que sea un lugar
+            for (let i = 0; i < data.hits.length; i++) {
+              const imageTags = data.hits[i].tags.toLowerCase();
+              // Verificar si las etiquetas incluyen la palabra "place" o "city"
+              if (imageTags.includes('places') || imageTags.includes('city') || imageTags.includes('capital')) {
+                setImageUrl(data.hits[i].webformatURL);
+                return; // Detener la iteración y salir del bucle cuando se encuentre la primera imagen de un lugar
+              }
+            }
+            setImageUrl(data.hits[0].webformatURL);
+            // Si no se encuentra imagen para no dejarla vacia poner una imagen por defecto igual referente al pais
+            // Si no se encontró ninguna imagen de un lugar, mostrar un mensaje de error
+            console.log(`No se encontraron imágenes de lugares para ${country.name}`);} else {
+          console.log(`No se encontraron imágenes para ${country.name}`);
+        }
+      })
+      .catch(error => console.error('Error fetching images:', error));
+  }, [country]);
+
   return (
-    <article className="card-info" key={country.code}>
-            <div style={imageStyles}>
-                <img
-                    src={'/templateImage.webp'}
+    <div className="card-info" >
+            <div style={imageStyles2}>
+            <img
+                    src={imageUrl}
                     fill={true}
                     alt={'Country Image'}
-                    className='info-country'
+                    className='image-card '
+                     
                 />
             </div>
+            
             <div className="country-info">
                 <img style={{ width: '37px' }} src={`https://flagicons.lipis.dev/flags/4x3/${country.code.toLowerCase()}.svg`} />
                 <div>
                     <h4>{country.name}</h4>
-                    <p>{country.capital}</p>
+                    <p>{country.continent.name}</p>
                 </div>
             </div>
-        </article>
+              <div>
+                <p><strong>Capital:</strong>   {country.capital}</p>
+                <p><strong>Languague:</strong>   {country?.languages?.[0]?.name}</p>
+                <p><strong>Currency:</strong>   {country.currency}</p>
+                <p><strong>Region:</strong>   {country.region}</p>
+
+              </div>
+            <div>
+            { <button onClick={close}>Cerrar</button>}
+            </div>
+        </div>
   )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,72 @@
-'use client'
-import { useEffect, useState } from "react";
-import CountryCard from "./components/CountryCard";
-import useCountries from "@/Hooks/consultCountry";
-import FilterContinent from "./components/FilterContinent";
+  'use client'
+  import { useEffect, useState } from "react";
+  import CountryCard from "./components/CountryCard";
+  import useCountries from "@/Hooks/consultCountry";
+  import FilterContinent from "./components/FilterContinent";
+  import CardInfo from "./components/CardInfo";
 
-function HomePage() {
-  const [inputValue, setInputValue] = useState("");
-  const {countries} = useCountries(); 
-  const [filterContinent, setFilterContinent] = useState(false);
+  function HomePage() {
+    const [inputValue, setInputValue] = useState("");
+    const {countries } = useCountries(); 
+    const [filterContinent, setFilterContinent] = useState(false);
+    const [selecCountry, setSelectCountry] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const countriesPerPage = 9;
 
-  return (
-    <>
-      <div>
+    const indexOfLastCountry = currentPage * countriesPerPage;
+    // Calcular el índice del primer país en la página actual
+    const indexOfFirstCountry = indexOfLastCountry - countriesPerPage;
+    // Obtener los países que se mostrarán en la página actual
+    const currentCountries = countries.slice(indexOfFirstCountry, indexOfLastCountry);
+  
+    
+    const paginateNext = () => {
+      if (indexOfLastCountry < countries.length) {
+        setCurrentPage(currentPage + 1);
+      }
+    };
+  
+    // Cambiar a la página anterior
+    const paginatePrev = () => {
+      if (currentPage > 1) {
+        setCurrentPage(currentPage - 1);
+      }
+    };
+
+    return (
+      <>
         <div>
-          <input className="search-input" type="text" placeholder="Encuentra tu pais" onChange={(e) => setInputValue(e.target.value)}></input>
-          <button onClick={() => setFilterContinent(!filterContinent)}>Filtrar por continente</button>
-        </div>
-        {filterContinent ? <FilterContinent/> : null}
-        
-        <section className="countries-grid">
+          <div className="search-container">
+            <input className="search-input" type="text" placeholder="Encuentra tu pais" onChange={(e) => setInputValue(e.target.value)}></input>
+            <button onClick={() => setFilterContinent(!filterContinent)}>Filtrar por continente</button>
+          </div>
+          {filterContinent ? <FilterContinent/> : null}
+          {console.log('countries since home', countries)}
+          <section className="countries-grid">
+            
+            {currentCountries.map((country) => {
+              if (country.name.toLowerCase().startsWith(inputValue.toLowerCase())) {
+                return <CountryCard
+                 onClick={() => setSelectCountry(country)}
+                 onClick2={() => setSelectCountry(false)}
+                 country={country} 
+                 key={country.code}/>
+              }else {
+                return null
+              }
+            })}
+          </section>
+          {selecCountry ? <CardInfo country={selecCountry} close={() => setSelectCountry(false)} /> : null}
           
-          {countries.map((country) => {
-            if (country.name.toLowerCase().startsWith(inputValue.toLowerCase())) {
-              return <CountryCard country={country} key={country.code}/>
-            }else {
-              return null
-            }
-          })}
-        </section>
-      </div>
-    </>
-  )
-}
+          <div>
+            <button onClick={paginatePrev} disabled={currentPage === 1}>Anterior</button>
+            <button onClick={paginateNext} disabled={indexOfLastCountry >= countries.length}>Siguiente</button>
+          </div>
 
-export default HomePage;
+
+        </div>
+      </>
+    )
+  }
+
+  export default HomePage;
